5379b188506d68ace550a1fe66e41e26596ddacf
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-10-19 17:11:51 +0530

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Neosoft on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
