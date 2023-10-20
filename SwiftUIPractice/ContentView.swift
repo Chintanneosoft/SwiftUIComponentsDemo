8310d5429ff830dc1748e6a0487a86426b3fc4f8
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-10-20 12:27:52 +0530

    Practice

--- SwiftUIPractice/ContentView.swift
+++ SwiftUIPractice/ContentView.swift
@@ -7,16 +7,119 @@
 
 import SwiftUI
 
+enum Flavor: String, CaseIterable, Identifiable {
+    case chocolate, vanilla, strawberry
+    var id: Self { self }
+}
+
 struct ContentView: View {
+    
+    @State private var selectedFlavor: Flavor = .chocolate
+    @State var selectedDate = Date()
+    @State var quantity: Int = 0
+    @State var isError: Bool = false
+    
+    var rows: [GridItem] =
+            Array(repeating: .init(.fixed(20)), count: 4)
+    
+    var dateClosedRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
+        let max = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
+        return min...max
+    }
+    
+    var modal: some View {
+        Text("Modal")
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List {
+                Picker("Flavor", selection: $selectedFlavor) {
+                    Text("Chocolate").tag(Flavor.chocolate)
+                    Text("Vanilla").tag(Flavor.vanilla)
+                    Text("Strawberry").tag(Flavor.strawberry)
+                }
+                
+                DatePicker(
+                    selection: $selectedDate,
+                    in: dateClosedRange,
+                    displayedComponents: [.hourAndMinute, .date],
+                    label: { Text("Due Date") }
+                )
+                
+                Stepper(value: $quantity, in: 0...10, label: { Text("Quantity \(quantity)")})
+                
+                HStack (alignment: .center, spacing: 20){
+                    Text("Hello")
+                    Divider()
+                    Text("World")
+                }.frame(maxWidth:.infinity)
+                
+                ScrollView(.horizontal) {
+                    LazyHStack(alignment: .center, spacing: 20) {
+                        ForEach(1...20, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                
+                ZStack {
+                    Text("World")
+                        .padding(20)
+                        .background(Color.red)
+                        .offset(x: 0, y: 30)
+                    Text("Hello")
+                        .padding(10)
+                        .background(Color.red)
+                        .opacity(0.8)
+                    
+                }.frame(maxWidth:.infinity)
+                
+                ScrollView(.horizontal) {
+                    LazyHGrid(rows: rows, alignment: .top) {
+                        ForEach((0...100), id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                
+                TabView {
+                    Text("First View")
+                        .font(.title)
+                        .tabItem({ Text("First") })
+                        .tag(0)
+                    Text("Second View")
+                        .font(.title)
+                        .tabItem({ Text("Second") })
+                        .tag(1)
+                }
+                
+                TabView {
+                    Text("1")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.pink)
+                    Text("2")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.red)
+                    Text("3")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.green)
+                    Text("4")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.blue)
+                }.tabViewStyle(PageTabViewStyle())
+                
+                Button("Alert") {
+                    self.isError = true
+                }.alert(isPresented: $isError, content: {
+                    Alert(title: Text("Alert"), message: Text("Alert Message"), dismissButton: .default(Text("OK")))
+                }).frame(maxWidth: .infinity)
+            }
         }
-        .padding()
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -24,3 +127,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
